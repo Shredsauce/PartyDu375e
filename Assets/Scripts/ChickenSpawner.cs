4d3de869466339402443439a00176f7ed347e713
Jeffxufen <clome2000@hotmail.com>
2017-05-27 14:57:16 -0400

    Chicken

---
+++ Assets/Scripts/ChickenSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenSpawner : MonoBehaviour {
+
+    public bool hasChicken;
+
+	// Use this for initialization
+	void Start () {
+        hasChicken = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawIcon(transform.position, "cone", true);
+    }
+}
