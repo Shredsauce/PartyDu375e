61945b095a93fea6a6efb83392592ec275cb0615
Ghyslain Godon <ld.ghyslain.godon@gmail.com>
2017-05-27 10:32:34 -0400

    Merge branch 'master' of https://github.com/Shredsauce/PartyDu375e

---
+++ Assets/Script/Cone.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cone : MonoBehaviour {
+    public GameObject car;
+
+	// Use this for initialization
+	void Start () {
+        car = FindObjectOfType<UnityStandardAssets.Vehicles.Car.CarController>().gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        //When the player car collides with a cone, enter the IF
+        if (other.gameObject.tag == "Player")
+        {
+            car.GetComponent<CarScoreManager>().coneRemoved += 1;
+            Destroy(this.gameObject);
+        }
+    }
+}
+
+
