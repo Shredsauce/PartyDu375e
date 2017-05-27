ff889992165f4a985ce36c84e8df2538c29434ba
Jeffxufen <clome2000@hotmail.com>
2017-05-27 19:11:41 -0400

    MainMenu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+    public  Canvas mainMenu;
+    public  Canvas credit;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+     public void Quit()
+    {
+        Application.Quit();
+           
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MTL2");
+    }
+    public void Back()
+    {
+        mainMenu.gameObject.SetActive(true);
+        credit.gameObject.SetActive(false);
+    
+    }
+    public void Credit()
+    {
+        mainMenu.gameObject.SetActive(false);
+        credit.gameObject.SetActive(true);
+    }
+}
