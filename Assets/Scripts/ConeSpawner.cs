1381feafab592b1b351afcdc1df6687d1588d39f
Malcolm Arcand Laliberté <malcolm@shredsauce.com>
2017-05-27 18:54:46 -0400

    ConeSpawner HasCone instead of hasCone

--- Assets/Scripts/ConeSpawner.cs
+++ Assets/Scripts/ConeSpawner.cs
@@ -3,20 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ConeSpawner : MonoBehaviour {
-
-    public bool hasCone;
-
-	// Use this for initialization
-	void Start () {
-        hasCone = false;
-
-    }   // Update is called once per frame
-    void Update () {
-		
+	private bool hasCone = false;
+	public bool HasCone {
+		get {
+			return hasCone;
+		}
+		set {
+			hasCone = value;
+		}
 	}
 
     void OnDrawGizmos()
     {
         Gizmos.DrawIcon(transform.position,"cone" , true);
     }
+
 }
