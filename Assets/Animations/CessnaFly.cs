3c1fbe7107faad1c7382ce5efe087639f5dd6384
Malcolm Arcand Laliberté <malcolm@shredsauce.com>
2017-05-27 21:51:27 -0400

    Cessna prefab added (Place above of the mountain)

---
+++ Assets/Animations/CessnaFly.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CessnaFly : MonoBehaviour {
+
+	private GameObject pivot;
+	[SerializeField] private float radius = 50f;
+	[SerializeField] private float tilt = 24f;
+	[SerializeField] private float flySpeed = 24f;
+
+	void Start () {
+		pivot = new GameObject("Airplane_pivot");
+		this.transform.parent = pivot.transform;
+
+		this.transform.localPosition = new Vector3 (this.transform.localPosition.x + radius, this.transform.localPosition.y, this.transform.localPosition.z);
+		this.transform.localEulerAngles = new Vector3 (0f, 0f, tilt);
+	}
+	
+	void Update () {
+		pivot.transform.Rotate(0f, flySpeed * Time.deltaTime, 0f);
+	}
+}
